59ae98042a98b683c294ae4da8d205aa941235d6
Fernando <fgutierrezara@gmail.com>
2026-03-11 19:30:28 +0100

    Limpiar repositorio aplicando .gitignore

--- Debug/Core/Src/control/rpm.d
+++
@@ -1,3 +0,0 @@
-Core/Src/control/rpm.o: ../Core/Src/control/rpm.c \
- ../Core/Inc/control/rpm.h
-../Core/Inc/control/rpm.h:
--- Debug/Core/Src/sensores/calibracion/calibracion.d
+++
@@ -1,4 +0,0 @@
-Core/Src/sensores/calibracion/calibracion.o: \
- ../Core/Src/sensores/calibracion/calibracion.c \
- ../Core/Inc/sensores/calibracion/calibracion.h
-../Core/Inc/sensores/calibracion/calibracion.h:
